918e2ede4ca39d3f8436cddb80332b79da16d38b
WIN-2QJDOS8CRCD\user <baxxromm@gmail.com>
2023-07-13 16:45:58 +0600

    added bg

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Main.module.css";
 
 const Main = () => {
-  return <div className={styles.img}>hello world</div>;
+  return (
+    <div>
+      <div className={styles.img}></div>
+    </div>
+  );
 };
 
 export default Main;
