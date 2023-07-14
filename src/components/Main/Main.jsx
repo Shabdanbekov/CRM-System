93fe3d58d847e218ffa203927f634c25d4ee474a
Adilet Umarbekov <Adilet.umarbekov4@gmail.com>
2023-07-14 13:01:53 +0600

    added logo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./Main.module.css";
+import Logo from "../images/Logo.svg"
 
 const Main = () => {
   return (
     <div>
-      <div className={styles.mainBG}></div>
+      <div className={styles.mainBG}>
+        <div className={styles.container}>
+          <div className={styles.logoDiv}><img src={Logo} className={styles.logo} alt="logo" /></div>
+
+        </div>
+      </div>
     </div>
   );
 };
