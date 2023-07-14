075080b45664e17b45d39fa51a18276f90191d1c
Adilet Umarbekov <Adilet.umarbekov4@gmail.com>
2023-07-14 18:36:37 +0600

    added style singUp

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Auth.module.css";
 import Logo from "../images/Logo.svg";
+import InputMask from "react-input-mask"
 
 const Auth = () => {
   return (
@@ -10,16 +11,26 @@ const Auth = () => {
           <img src={Logo} alt="logo" className={styles.logo} />
         </div>
         <div className={styles.form}>
-          <label htmlFor="">Имя</label>
-          <input type="text" />
-          <label htmlFor="">Фамилия</label>
-          <input type="text" />
-          <label htmlFor="">Номер телефона</label>
+          <label   htmlFor="#">Имя</label>
+          <input placeholder="Иван" type="text" />
+          <label  htmlFor="#">Фамилия</label>
+          <input placeholder="Иванов" type="text" />
+          <label  htmlFor="#">Номер телефона</label>
+          <InputMask 
+      mask='(+996) 999 99 99 99' 
+      >
+    </InputMask>
+          <label  htmlFor="#">Дата рождения</label>
           <input type="number" />
-          <label htmlFor="">Дата рождения</label>
-          <input type="number" />
-          <label htmlFor="">Пароль</label>
+          <label  htmlFor="#">Пароль</label>
           <input type="password" />
+          <div className={styles.checkBox}>
+          <input className={styles.radio } type="radio" /> <div> 
+            <p className={styles.agrement}>Я прочитал и согласен с условиями </p>
+            <a href="#" className={styles.agrementLink}>Пользовательского соглашения</a>
+          </div>
+          </div>
+        
         </div>
       </div>
     </div>
